aed2beb8b08e777a0921859cbc488dc49c26ec7a
Cian McLennan <cmclennan@interactiveservices.com>
2016-04-25 13:34:58 +0100

    Initial Commit

---
+++ WindowsPathConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WindowsPathConverter
+//
+//  Created by Cian McLennan on 25/04/2016.
+//  Copyright © 2016 Cian McLennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WindowsPathConverter/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WindowsPathConverter
+//
+//  Created by Cian McLennan on 25/04/2016.
+//  Copyright © 2016 Cian McLennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
