56f8fdf8d647e7db9d324725c93c9a092c87bce8
Cian McLennan <ciankm88@gmail.com>
2016-08-31 20:47:11 +0100

    Added sparkle framework.

---
+++ WindowsPathConverter/MainWindowViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainWindowViewController.h
+//  WindowsPathConverter
+//
+//  Created by Cian McLennan on 31/08/2016.
+//  Copyright © 2016 Cian McLennan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainWindowViewController : NSViewController
+
+@end
---
+++ WindowsPathConverter/MainWindowViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainWindowViewController.m
+//  WindowsPathConverter
+//
+//  Created by Cian McLennan on 31/08/2016.
+//  Copyright © 2016 Cian McLennan. All rights reserved.
+//
+
+#import "MainWindowViewController.h"
+
+@interface MainWindowViewController ()
+
+@end
+
+@implementation MainWindowViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
